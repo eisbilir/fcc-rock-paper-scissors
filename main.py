9d654b51913e6beb80b2cf6dac94cc84a3c7f23d
Emre İŞBİLİR <emre.isbilir@gmail.com>
2020-11-07 21:24:21 +0300

    completed

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
 
-play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+# play(player, quincy, 1000)
+# play(player, abbey, 1000)
+# play(player, kris, 1000)
+# play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
@@ -16,4 +16,4 @@ play(player, mrugesh, 1000)
 
 
 # Uncomment line below to run unit tests automatically
-# main(module='test_module', exit=False)
+main(module="test_module", exit=False)
